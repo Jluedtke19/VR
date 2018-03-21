236332fa18968f27a66e270bc679e5aa0f0da02b
jjproby <jjproby719@gmail.com>
2018-03-21 09:30:59 -0500

    Changes Roll a Ball into a awful sonic experience

---
+++ RollABoll/Assets/Scripts/MusicPlay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlay : MonoBehaviour {
+
+    public AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+	}
+
+}
